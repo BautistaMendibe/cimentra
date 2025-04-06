a326cff96ce51a138ceabf6390ea36ba2184b920
bauti <bautistamendibe10@gmail.com>
2025-04-06 18:44:33 -0300

    btn para cerrar menu

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 import { ThemeSwitcher } from "./theme-switcher";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export default async function AuthButton() {
   const supabase = await createClient();
@@ -11,15 +12,27 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
   return user ? (
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
-          Sign out
-        </Button>
-      </form>
-      <ThemeSwitcher />
+
+    <div className="flex w-full items-center justify-between">
+      {/* Izquierda - icono menú */}
+      <div className="flex items-center">
+        <SidebarTrigger />
+      </div>
+
+      {/* Derecha - email, logout, theme */}
+      <div className="flex items-center gap-4">
+        <span className="text-sm">Hey, {user.email}!</span>
+        <form action={signOutAction}>
+          <Button type="submit" variant="outline">
+            Sign out
+          </Button>
+        </form>
+        <ThemeSwitcher />
+      </div>
     </div>
+
+
+
   ) : (
     <div className="flex gap-2">
       <Button asChild size="sm" variant={"outline"}>
